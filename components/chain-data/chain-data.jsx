f007b7f05be45cee61aeacc8a9710b6e26b263a9
panoplied <panoplied@protonmail.com>
2022-07-20 21:00:25 +0300

    Convert wei to gwei

--- components/chain-data/chain-data.jsx
+++ components/chain-data/chain-data.jsx
@@ -1,6 +1,7 @@
 import { useChain } from '../../lib/gas-fetcher';
 import { usePrice } from '../../lib/usd-fetcher';
 import { chains } from '../../lib/settings';
+import { ethers } from 'ethers';
 
 function ChainData({ chain }) {
 
@@ -9,55 +10,59 @@ function ChainData({ chain }) {
   const { data: gasData, isLoading: gasLoading, isError: gasError } = useChain(chain);
   const { data: usdData, isLoading: usdLoading, isError: usdError } = usePrice(chain);
 
-  if (gasLoading) return <p>gas loading...</p>
-  if (gasError) return <p>failed to load gas</p>
-  if (usdLoading) return <p>usd loading...</p>
-  if (usdError) return <p>failed to load USD value</p>
-
-  return (
-    <div style={{color: "white"}}>
-      <h2>{name}</h2>
-      <div style={{lineHeight: "0.75em"}}>
-        <p>⏱️ {gasData._seconds}</p>
-        <p>🐢 {gasData.data.slow.price}</p>
-        <p>🚗 {gasData.data.normal.price}</p>
-        <p>🚀 {gasData.data.fast.price}</p>
-        <p>💲 {usdData[usdSymbol].usd}</p>
-      </div>
-    </div>
-  );
-
-  // // Only destructure and assign when data loaded without errors
-  // const {
-  //   _seconds: seconds,
-  //   data: {
-  //     slow: {
-  //       price: priceSlow
-  //     },
-  //     normal: {
-  //       price: priceNormal
-  //     },
-  //     fast: {
-  //       price: priceFast,
-  //     },
-  //   }
-  // } = gasData;
-
-  // const { [usdSymbol]: { usd: usdValue } } = usdData;
+  if (gasLoading) return <p>Gas loading...</p>
+  if (gasError) return <p>Failed to load gas</p>
+  if (usdLoading) return <p>USD loading...</p>
+  if (usdError) return <p>Failed to load USD value</p>
 
   // return (
   //   <div style={{color: "white"}}>
   //     <h2>{name}</h2>
   //     <div style={{lineHeight: "0.75em"}}>
-  //       <p>⏱️ seconds: {seconds}</p>
-  //       <p>🐢 slow: {priceSlow}</p>
-  //       <p>🚗 normal: {priceNormal}</p>
-  //       <p>🚀 fast: {priceFast}</p>
-  //       <p>💲 USD: {usdValue}</p>
+  //       <p>⏱️ {gasData._seconds}</p>
+  //       <p>🐢 {gasData.data.slow.price}</p>
+  //       <p>🚗 {gasData.data.normal.price}</p>
+  //       <p>🚀 {gasData.data.fast.price}</p>
+  //       <p>💲 {usdData[usdSymbol].usd}</p>
   //     </div>
   //   </div>
   // );
 
+  // Only destructure and assign when data loaded without errors
+  const {
+    _seconds: seconds,
+    data: {
+      slow: {
+        price: priceSlow
+      },
+      normal: {
+        price: priceNormal
+      },
+      fast: {
+        price: priceFast,
+      },
+    }
+  } = gasData;
+
+  const { [usdSymbol]: { usd: usdValue } } = usdData;
+
+  return (
+    <div style={{color: "white"}}>
+      <h2>{name}</h2>
+      <div style={{lineHeight: "0.75em"}}>
+        {/* <p>⏱️ seconds: {seconds}</p> */}
+        <p>🐢 slow: {weiToGwei(priceSlow)}</p>
+        <p>🚗 normal: {weiToGwei(priceNormal)}</p>
+        <p>🚀 fast: {weiToGwei(priceFast)}</p>
+        <p>💲 USD: {usdValue}</p>
+      </div>
+    </div>
+  );
+
+}
+
+function weiToGwei(price) {
+  return ethers.utils.formatUnits(price, "gwei");
 }
 
 export default ChainData;
