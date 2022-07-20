30c7edfe7be551b82e810138b567a4cf8307e24b
panoplied <panoplied@protonmail.com>
2022-07-20 05:38:11 +0300

    Add emojis just for fun

--- components/chain-data/chain-data.jsx
+++ components/chain-data/chain-data.jsx
@@ -11,11 +11,11 @@ function ChainData({ chain }) {
   return (
     <div style={{color: "white"}}>
       <h2>{chains[chain].name}</h2>
-      <div style={{}}>
-        <p>Seconds: {data._seconds}</p>
-        <p>Slow: {data.data.slow.price}</p>
-        <p>Normal: {data.data.normal.price}</p>
-        <p>Fast: {data.data.fast.price}</p>
+      <div style={{lineHeight: "0.75em"}}>
+        <p>⏱️ seconds: {data._seconds}</p>
+        <p>🐢 slow: {data.data.slow.price}</p>
+        <p>🚗 normal: {data.data.normal.price}</p>
+        <p>🚀 fast: {data.data.fast.price}</p>
       </div>
       {/* <p style={{fontWeight: "bold"}}>{JSON.stringify(data)}</p> */}
     </div>
