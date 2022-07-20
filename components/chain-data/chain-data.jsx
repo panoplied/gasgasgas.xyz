8592a761acc1dfb60787478d22b062a43e9cbb75
panoplied <panoplied@protonmail.com>
2022-07-20 18:49:10 +0300

    Add USD price draft

--- components/chain-data/chain-data.jsx
+++ components/chain-data/chain-data.jsx
@@ -1,25 +1,60 @@
 import { useChain } from '../../lib/gas-fetcher';
+import { usePrice } from '../../lib/usd-fetcher';
 import { chains } from '../../lib/settings';
 
 function ChainData({ chain }) {
 
-  const { data, isLoading, isError } = useChain(chain);
+  const { name, usdSymbol } = chains[chain];
 
-  if (isLoading) return <p>loading...</p>
-  if (isError) return <p>failed to load</p>
+  const { data: gasData, isLoading: gasLoading, isError: gasError } = useChain(chain);
+  const { data: usdData, isLoading: usdLoading, isError: usdError } = usePrice(chain);
+
+  if (gasLoading) return <p>gas loading...</p>
+  if (gasError) return <p>failed to load gas</p>
 
   return (
     <div style={{color: "white"}}>
-      <h2>{chains[chain].name}</h2>
+      <h2>{name}</h2>
       <div style={{lineHeight: "0.75em"}}>
-        <p>⏱️ seconds: {data._seconds}</p>
-        <p>🐢 slow: {data.data.slow.price}</p>
-        <p>🚗 normal: {data.data.normal.price}</p>
-        <p>🚀 fast: {data.data.fast.price}</p>
+        <p>⏱️ seconds: {gasData._seconds}</p>
+        <p>🐢 slow: {gasData.data.slow.price}</p>
+        <p>🚗 normal: {gasData.data.normal.price}</p>
+        <p>🚀 fast: {gasData.data.fast.price}</p>
+        <p>💲 USD: {usdData[usdSymbol].usd}</p>
       </div>
-      {/* <p style={{fontWeight: "bold"}}>{JSON.stringify(data)}</p> */}
     </div>
   );
+
+  // // Only destructure and assign when data loaded without errors
+  // const {
+  //   _seconds: seconds,
+  //   data: {
+  //     slow: {
+  //       price: priceSlow
+  //     },
+  //     normal: {
+  //       price: priceNormal
+  //     },
+  //     fast: {
+  //       price: priceFast,
+  //     },
+  //   }
+  // } = gasData;
+
+  // const { [usdSymbol]: { usd: usdValue } } = usdData;
+
+  // return (
+  //   <div style={{color: "white"}}>
+  //     <h2>{name}</h2>
+  //     <div style={{lineHeight: "0.75em"}}>
+  //       <p>⏱️ seconds: {seconds}</p>
+  //       <p>🐢 slow: {priceSlow}</p>
+  //       <p>🚗 normal: {priceNormal}</p>
+  //       <p>🚀 fast: {priceFast}</p>
+  //       <p>💲 USD: {usdValue}</p>
+  //     </div>
+  //   </div>
+  // );
 
 }
 
