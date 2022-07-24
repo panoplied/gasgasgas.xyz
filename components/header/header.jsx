0abe89faedebf61125734613e723fac48c319c05
panoplied <panoplied@protonmail.com>
2022-07-24 03:44:15 +0300

    Set select none for the "logo"

--- components/header/header.jsx
+++ components/header/header.jsx
@@ -1,11 +1,12 @@
 import ThemeToggle from '../theme-toggle';
 
 function Header() {
+
   return (
     <div className="flex w-full p-4">
-      <h1 className="text-4xl grow self-center">⛽</h1>
+      <h1 className="text-4xl grow self-center select-none">⛽</h1>
       {/* TODO: Implement input for custom gas limit */}
-      {/* <input className="grow h-16 justify-self-center"></input> */}
+      <input className="grow h-16 justify-self-center"></input>
       <div className="grow flex self-center flex-row-reverse">
         <ThemeToggle />
       </div>
