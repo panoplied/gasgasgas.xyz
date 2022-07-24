76bd6adb5578903a606002b6de0bb7a4d5454a77
panoplied <panoplied@protonmail.com>
2022-07-25 01:27:26 +0300

    Make default theme dark; based on user testing

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider enableSystem={true} attribute="class">
+    <ThemeProvider defaultTheme="dark" attribute="class">
       <Component {...pageProps} />
     </ThemeProvider>
   );
