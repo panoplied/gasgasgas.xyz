204281e434033870c28d289bbf31cb5df75163c8
panoplied <panoplied@protonmail.com>
2022-07-28 00:34:10 +0300

    SSG for everything except fee calculations (obviously)

--- components/chains-grid/chains-grid.jsx
+++ components/chains-grid/chains-grid.jsx
@@ -1,23 +1,16 @@
 import ChainData from '../chain-data';
 
-function ChainsGrid() {
+function ChainsGrid({ chains, speedMarks }) {
 
-  // Use chains from lib/settings.js as chain props
+  // Using chains from lib/settings.js as chain props
+
   return (
     <div className="p-4 max-w-5xl grid gap-6 xs:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 min-w-[80%]">
-      <ChainData chain="eth" />
-      <ChainData chain="bsc" />
-      <ChainData chain="xdai" />
-      <ChainData chain="matic" />
-      <ChainData chain="ftm" />
-      <ChainData chain="okt" />
-      <ChainData chain="avax" />
-      <ChainData chain="op" />
-      <ChainData chain="arb" />
-      <ChainData chain="celo" />
-      <ChainData chain="movr" />
-      <ChainData chain="cro" />
-      <ChainData chain="boba" />
+      {
+        Object.values(chains).map(chain => (
+          <ChainData chain={chain} speedMarks={speedMarks} key={chain.id} />
+        ))
+      }
     </div>
   );
 
--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,9 @@ import Header from '../components/header';
 import BackgroundImage from '../components/background-image';
 
 import ChainsGrid from '../components/chains-grid';
+import { chains, speedMarks } from '../lib/settings';
 
-export default function Home() {
+export function Home({ chains, speedMarks}) {
 
   return (
     <>
@@ -30,9 +31,20 @@ export default function Home() {
 
       <main className="grid place-items-center min-h-screen bg-zinc-50/50 dark:bg-zinc-900/50">
         <Header />
-        <ChainsGrid />
+        <ChainsGrid chains={chains} speedMarks={speedMarks} />
       </main>
 
     </>
   );
 }
+
+export async function getStaticProps() {
+  return {
+    props: {
+      chains,
+      speedMarks,
+    },
+  };
+}
+
+export default Home;
